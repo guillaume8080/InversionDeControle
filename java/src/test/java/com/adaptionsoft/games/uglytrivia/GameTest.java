af84831c0405aef0a770810fcf2b7c40c3a1b74a
Guillaume Gouy <guillaumegouy@sfr.fr>
2022-03-31 15:35:19 +0200

    first commit

---
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -0,0 +1,14 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void roll() {
+        assertEquals(1,1);
+    }
+}
